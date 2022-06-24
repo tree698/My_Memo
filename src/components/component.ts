1c3ce0b6e058c7cecd821dfec77543e1624a78e4
Chanwoo <tree698@gmail.com>
2022-06-24 16:31:37 +0900

    add functionality of close button

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -10,7 +11,14 @@ export class BaseComponent<T extends HTMLElement> implements Component {
     this.element = template.content.firstElementChild! as T;
   }
 
-  attachTo(parent: T, position: InsertPosition = 'afterbegin') {
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement) {
+    if (parent !== this.element.parentElement) {
+      throw new Error('Parent mismatch');
+    }
+    parent.removeChild(this.element);
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,14 +4,21 @@ export interface Composable {
   addChild(item: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 export class PageItemComponent extends BaseComponent<HTMLElement> {
+  private closeListener?: OnCloseListener;
   constructor() {
     super(`<li class="page-item">
             <div class="page-item__body"></div>
-            <div class="page-item__control">
-              <button>&times;</button>
+            <div class="page-item__controls">
+              <button class="close">&times;</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(item: Component) {
@@ -20,18 +27,26 @@ export class PageItemComponent extends BaseComponent<HTMLElement> {
     )! as HTMLElement;
     item.attachTo(pageItemBody);
   }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
 
 export class PageComponent extends BaseComponent<HTMLElement> {
   constructor() {
-    super(`<ul class="page">This is a Page</ul>`);
+    super(`<ul class="page"></ul>`);
   }
 
   addChild(item: Component) {
     const pageItem = new PageItemComponent();
-    // 각 item을 pageItem(li) body에 추가
+    // item을 pageItem(li) body에 추가
     pageItem.addChild(item);
     // pageItem(li)를 this.element(ul)에 추가
     pageItem.attachTo(this.element, 'beforeend');
+
+    pageItem.setOnCloseListener(() => {
+      pageItem.removeFrom(this.element);
+    });
   }
 }
